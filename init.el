540d59e9bb0e5dbaa1be11753f47bb996af2b182
glenn <glenn.eckstein@gmail.com>
2024-12-19 21:54:10 -0600

    new laptop, set up org-roam and agenda

--- init.el
+++ init.el
@@ -1,5 +1,7 @@
 ;;(eval-after-load "org"
 ;;  '(debug))
+
+;;On first load this file wont exist, emacs.org must be tangled to create emacs.el
 (load "~/.emacs.d/emacs.el")
 
 ;;(org-babel-load-file "~/.emacs.d/emacs.org")
